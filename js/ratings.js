32ede0ea22bd2f40ef3e1f13f04bc056eec708a8
Erubore Mildred <87650370+mildred366@users.noreply.github.com>
2021-11-09 23:57:03 +0100

    git project on JS calc

---
+++ js/ratings.js
@@ -0,0 +1,25 @@
+function collect_ratings() {
+    const ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+    let rating = 0;
+const elements = documents.querySelectorAll(".rating");
+
+elements.forEach(element => {
+    rating = parseInt(element.id.replace("star", " "));
+     ratings.count += parseInt(element.value);
+     ratings.sum += parseInt(elements.value) * rating;
+    });  
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+        return ratings
+    };
+    document.addEventListener("change", () => {
+        const ratings = collect_ratings;
+    document.querySelector("#average").value=ratings.average.toFixed(2);
+    });
+
+    
