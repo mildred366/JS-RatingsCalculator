e31888f2d827ef6813d089f50e1c9a4ab591f529
Erubore Mildred <87650370+mildred366@users.noreply.github.com>
2021-11-10 00:07:37 +0100

    made a mistake in the querySelector method

--- js/ratings.js
+++ js/ratings.js
@@ -5,7 +5,7 @@ function collect_ratings() {
         average: 0
     };
     let rating = 0;
-const elements = documents.querySelectorAll(".rating");
+const elements = document.querySelectorAll(".rating");
 
 elements.forEach(element => {
     rating = parseInt(element.id.replace("star", " "));
